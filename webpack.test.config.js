e1261e6d89a1115979809b63a18280bbce891978
hiep tran <hoanghieptran86@gmail.com>
2019-08-23 00:48:31 +0700

    Update webpack.test.config.js

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
 module.exports = {
   // mode: 'development',
+    entry: './lib/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        //test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
